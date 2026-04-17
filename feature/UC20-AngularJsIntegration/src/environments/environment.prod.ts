d2203bc576220d7585acf481d9f5e69af17cc98b
ojas2005 <tiwariojas4@gmail.com>
2026-04-17 11:45:38 +0530

    Fix deployment: base href, routing, render.yaml

--- feature/UC20-AngularJsIntegration/src/environments/environment.prod.ts
+++ feature/UC20-AngularJsIntegration/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBase: 'https://your-production-api.com'  
+  apiBase: 'http://quantitymanagementapp.onrender.com'  
 };
--- feature/UC20-AngularJsIntegration/src/environments/environment.ts
+++ feature/UC20-AngularJsIntegration/src/environments/environment.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:5000'   
+  apiBase: 'https://quantitymanagementapp.onrender.com'   
 };
+
